c27f23ced8df5ce0b0f60e852ef676cf57f4a0d9
darren du <darren_l_du@manulife.com>
2024-01-02 20:40:57 +0800

    init

--- main.py
+++ main.py
@@ -4,13 +4,10 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+from flask import Flask
 
+app = Flask(__name__)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
